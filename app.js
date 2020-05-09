c8aaaecee31e9654e57d30489ff798481821e98b
Mohan Kumar <46491614+imshines@users.noreply.github.com>
2020-05-09 19:13:58 +0530

    completed :)

---
+++ app.js
@@ -0,0 +1,21 @@
+let body = document.querySelector('body');
+let generateBtn = document.querySelector('.generate');
+let gradientValues = document.querySelector('.gradient-values');
+
+generateBtn.addEventListener('click', () => {
+    this.location.reload();
+})
+
+let r1 = Math.round(Math.random() * 255);
+let g1 = Math.round(Math.random() * 255);
+let b1 = Math.round(Math.random() * 255);
+let r2 = Math.round(Math.random() * 255);
+let g2 = Math.round(Math.random() * 255);
+let b2 = Math.round(Math.random() * 255);
+let dir = ['to left', 'to right', 'to top', 'to bottom'];
+let index = Math.floor(Math.round(Math.random() * 3));
+console.log(index);
+
+body.style.backgroundImage = `linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))`;
+
+gradientValues.innerHTML = `<code>linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))</code>`;
---
+++ readme.md
@@ -0,0 +1,5 @@
+# CSS Gradient Generator
+
+## -> Just a simple CSS generator with VanillaJS
+
+### Check out here https://imshines.github.io/gradient-generator 

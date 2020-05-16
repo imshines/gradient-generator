546603f0725534a548b099bbb6f69e6babc9ac7a
Mohan Kumar <46491614+imshines@users.noreply.github.com>
2020-05-16 14:32:47 +0530

    completed :)

--- app.js
+++ app.js
@@ -2,20 +2,19 @@ let body = document.querySelector('body');
 let generateBtn = document.querySelector('.generate');
 let gradientValues = document.querySelector('.gradient-values');
 
-generateBtn.addEventListener('click', () => {
-    this.location.reload();
-})
+generateBtn.addEventListener('click', refresh)
 
-let r1 = Math.round(Math.random() * 255);
-let g1 = Math.round(Math.random() * 255);
-let b1 = Math.round(Math.random() * 255);
-let r2 = Math.round(Math.random() * 255);
-let g2 = Math.round(Math.random() * 255);
-let b2 = Math.round(Math.random() * 255);
-let dir = ['to left', 'to right', 'to top', 'to bottom'];
-let index = Math.floor(Math.round(Math.random() * 3));
-console.log(index);
+function refresh() {
+    let r1 = Math.round(Math.random() * 255);
+    let g1 = Math.round(Math.random() * 255);
+    let b1 = Math.round(Math.random() * 255);
+    let r2 = Math.round(Math.random() * 255);
+    let g2 = Math.round(Math.random() * 255);
+    let b2 = Math.round(Math.random() * 255);
+    let dir = ['to left', 'to right', 'to top', 'to bottom'];
+    let index = Math.floor(Math.round(Math.random() * 3));
+    body.style.backgroundImage = `linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))`;
+    gradientValues.innerHTML = `<code>linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))</code>`;
+}
 
-body.style.backgroundImage = `linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))`;
-
-gradientValues.innerHTML = `<code>linear-gradient(${dir[index]},rgb(${r1},${g1},${b1}),rgb(${r2},${g2},${b2}))</code>`;
+refresh();
